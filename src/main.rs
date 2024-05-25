9295909ed5e7813aac40c4ce37b999f91d344128
cdrmack <git.pynvt@passmail.net>
2024-05-25 09:27:41 +0200

    add server, always respond with error for now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::{TcpListener, TcpStream};
+use mini_redis::{Connection, Frame};
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    // second item contains the IP and port of the new connection
+    loop {
+	let (socket, _) = listener.accept().await.unwrap();
+	process(socket).await;
+    }
+}
+
+async fn process(socket: TcpStream) {
+    // read/write redis frames instead of byte streams
+    let mut connection = Connection::new(socket);
+
+    if let Some(frame) = connection.read_frame().await.unwrap() {
+	println!("GOT: {:?}", frame);
+
+	let response = Frame::Error("unimplemented".to_string());
+	connection.write_frame(&response).await.unwrap();
+    }
 }
