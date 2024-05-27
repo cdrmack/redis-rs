7ec1e9bdec39234a7d30126f044cbf49ab980566
cdrmack <git.pynvt@passmail.net>
2024-05-27 14:24:50 +0200

    add Mutex for shared data

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,34 @@
 use tokio::net::{TcpListener, TcpStream};
 use mini_redis::{Connection, Frame};
+use bytes::Bytes;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
+    println!("Listening");
+
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
     // second item contains the IP and port of the new connection
     loop {
 	let (socket, _) = listener.accept().await.unwrap();
+
+	let db = db.clone();
+
+	println!("Accepted");
 	tokio::spawn(async move {
-	    process(socket).await;
+	    process(socket, db).await;
 	});
     }
 }
 
-async fn process(socket: TcpStream) {
-    use std::collections::HashMap;
+async fn process(socket: TcpStream, db: Db) {
     use mini_redis::Command::{self, Get, Set};
-
-    let mut db = HashMap::new();
 
     // read/write redis frames instead of byte streams
     let mut connection = Connection::new(socket);
@@ -26,12 +36,14 @@ async fn process(socket: TcpStream) {
     while let Some(frame) = connection.read_frame().await.unwrap() {
 	let response = match Command::from_frame(frame).unwrap() {
 	    Set(cmd) => {
-		db.insert(cmd.key().to_string(), cmd.value().to_vec());
+		let mut db = db.lock().unwrap();
+		db.insert(cmd.key().to_string(), cmd.value().clone());
 		Frame::Simple("OK".to_string())
 	    }
 	    Get(cmd) => {
+		let db = db.lock().unwrap();
 		if let Some(value) = db.get(cmd.key()) {
-		    Frame::Bulk(value.clone().into())
+		    Frame::Bulk(value.clone())
 		}
 		else {
 		    Frame::Null
