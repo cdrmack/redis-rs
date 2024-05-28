148785a140acc3cdc15212e73bedeb49507f898e
cdrmack <git.pynvt@passmail.net>
2024-05-28 17:25:46 +0200

    use mpsc (client)

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,17 +1,20 @@
-use mini_redis::client;
+use tokio::sync::mpsc;
+
 
 #[tokio::main]
 async fn main() {
-    let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+    let (tx, mut rx) = mpsc::channel(32);
+    let tx2 = tx.clone();
 
-    let t1 = tokio::spawn(async {
-	let res = client.get("foo");
+    tokio::spawn(async move {
+	tx.send("sending from first handle").await.unwrap();
     });
 
-    let t2 = tokio::spawn(async {
-	client.set("foo", "bar".into()).await;
+    tokio::spawn(async move {
+	tx2.send("sending from second handle").await.unwrap();
     });
 
-    t1.await.unwrap();
-    t2.await.unwrap();
+    while let Some(message) = rx.recv().await {
+	println!("GOT = {}", message);
+    }
 }
