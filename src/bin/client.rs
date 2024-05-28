5e71631eef4713b920e5088f2f92563e1a0fca32
cdrmack <git.pynvt@passmail.net>
2024-05-28 18:22:24 +0200

    add Command enum

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,4 +1,17 @@
 use tokio::sync::mpsc;
+use mini_redis::client;
+use bytes::Bytes;
+
+#[derive(Debug)]
+enum Command {
+    Get {
+	key: String,
+    },
+    Set {
+	key: String,
+	val: Bytes,
+    }
+}
 
 
 #[tokio::main]
@@ -6,15 +19,41 @@ async fn main() {
     let (tx, mut rx) = mpsc::channel(32);
     let tx2 = tx.clone();
 
-    tokio::spawn(async move {
-	tx.send("sending from first handle").await.unwrap();
+    let t1 = tokio::spawn(async move {
+	let cmd = Command::Get {
+	    key: "foo".to_string(),
+	};
+
+	tx.send(cmd).await.unwrap();
     });
 
-    tokio::spawn(async move {
-	tx2.send("sending from second handle").await.unwrap();
+    let t2 = tokio::spawn(async move {
+	let cmd = Command::Set {
+	    key: "foo".to_string(),
+	    val: "bar".into(),
+	};
+
+	tx2.send(cmd).await.unwrap();
     });
 
-    while let Some(message) = rx.recv().await {
-	println!("GOT = {}", message);
-    }
+    let manager = tokio::spawn(async move {
+	let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+
+	while let Some(cmd) = rx.recv().await {
+	    use Command::*;
+
+	    match cmd {
+		Get { key } => {
+		    let _ = client.get(&key).await;
+		}
+		Set { key, val } => {
+		    let _ = client.set(&key, val).await;
+		}
+	    }
+	}
+    });
+
+    t1.await.unwrap();
+    t2.await.unwrap();
+    manager.await.unwrap();
 }
